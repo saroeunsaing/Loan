884397c122b61d0f5d092a2421e3ed33bd69f5ec
GURUTECH <guru.tech.kh.com>
2021-10-23 20:35:36 +0700

    Form Credit type

---
+++ Loan/FORMS/Frm_CreditType.Designer.cs
@@ -0,0 +1,315 @@
+﻿
+namespace Loan.FORMS
+{
+    partial class Frm_CreditType
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Btn_Exit = new FontAwesome.Sharp.IconButton();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.iconButton4 = new FontAwesome.Sharp.IconButton();
+            this.TXT_USERNAME = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.iconButton2 = new FontAwesome.Sharp.IconButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.DGV_DATA = new System.Windows.Forms.DataGridView();
+            this.LOGIN = new FontAwesome.Sharp.IconButton();
+            this.iconButton3 = new FontAwesome.Sharp.IconButton();
+            this.iconButton5 = new FontAwesome.Sharp.IconButton();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_DATA)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.panel1.Controls.Add(this.iconButton5);
+            this.panel1.Controls.Add(this.iconButton3);
+            this.panel1.Controls.Add(this.LOGIN);
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.iconButton2);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.iconButton1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.iconButton4);
+            this.panel1.Controls.Add(this.TXT_USERNAME);
+            this.panel1.Controls.Add(this.Btn_Exit);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 300);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.DGV_DATA);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 300);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(600, 300);
+            this.panel2.TabIndex = 1;
+            // 
+            // Btn_Exit
+            // 
+            this.Btn_Exit.FlatAppearance.BorderSize = 0;
+            this.Btn_Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Exit.Font = new System.Drawing.Font("Khmer OS Siemreap", 10F);
+            this.Btn_Exit.ForeColor = System.Drawing.Color.MistyRose;
+            this.Btn_Exit.IconChar = FontAwesome.Sharp.IconChar.WindowClose;
+            this.Btn_Exit.IconColor = System.Drawing.Color.LightCoral;
+            this.Btn_Exit.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.Btn_Exit.IconSize = 30;
+            this.Btn_Exit.Location = new System.Drawing.Point(562, 3);
+            this.Btn_Exit.Name = "Btn_Exit";
+            this.Btn_Exit.Size = new System.Drawing.Size(35, 35);
+            this.Btn_Exit.TabIndex = 1;
+            this.Btn_Exit.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel3.ForeColor = System.Drawing.Color.MistyRose;
+            this.panel3.Location = new System.Drawing.Point(28, 89);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(383, 3);
+            this.panel3.TabIndex = 6;
+            // 
+            // iconButton4
+            // 
+            this.iconButton4.FlatAppearance.BorderSize = 0;
+            this.iconButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton4.Font = new System.Drawing.Font("Khmer OS Siemreap", 10F);
+            this.iconButton4.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.iconButton4.IconChar = FontAwesome.Sharp.IconChar.AddressBook;
+            this.iconButton4.IconColor = System.Drawing.Color.WhiteSmoke;
+            this.iconButton4.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton4.IconSize = 30;
+            this.iconButton4.Location = new System.Drawing.Point(29, 55);
+            this.iconButton4.Name = "iconButton4";
+            this.iconButton4.Size = new System.Drawing.Size(32, 35);
+            this.iconButton4.TabIndex = 7;
+            this.iconButton4.UseVisualStyleBackColor = true;
+            // 
+            // TXT_USERNAME
+            // 
+            this.TXT_USERNAME.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.TXT_USERNAME.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TXT_USERNAME.Font = new System.Drawing.Font("Khmer OS Siemreap", 12F);
+            this.TXT_USERNAME.Location = new System.Drawing.Point(70, 56);
+            this.TXT_USERNAME.Name = "TXT_USERNAME";
+            this.TXT_USERNAME.Size = new System.Drawing.Size(339, 30);
+            this.TXT_USERNAME.TabIndex = 5;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel4.ForeColor = System.Drawing.Color.MistyRose;
+            this.panel4.Location = new System.Drawing.Point(28, 166);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(383, 3);
+            this.panel4.TabIndex = 9;
+            // 
+            // iconButton1
+            // 
+            this.iconButton1.FlatAppearance.BorderSize = 0;
+            this.iconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton1.Font = new System.Drawing.Font("Khmer OS Siemreap", 10F);
+            this.iconButton1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.SortAlphaDown;
+            this.iconButton1.IconColor = System.Drawing.Color.WhiteSmoke;
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton1.IconSize = 30;
+            this.iconButton1.Location = new System.Drawing.Point(29, 132);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Size = new System.Drawing.Size(32, 35);
+            this.iconButton1.TabIndex = 10;
+            this.iconButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Khmer OS Siemreap", 12F);
+            this.textBox1.Location = new System.Drawing.Point(70, 133);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(339, 30);
+            this.textBox1.TabIndex = 8;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel5.ForeColor = System.Drawing.Color.MistyRose;
+            this.panel5.Location = new System.Drawing.Point(28, 239);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(383, 3);
+            this.panel5.TabIndex = 12;
+            // 
+            // iconButton2
+            // 
+            this.iconButton2.FlatAppearance.BorderSize = 0;
+            this.iconButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton2.Font = new System.Drawing.Font("Khmer OS Siemreap", 10F);
+            this.iconButton2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.iconButton2.IconChar = FontAwesome.Sharp.IconChar.SortAlphaUpAlt;
+            this.iconButton2.IconColor = System.Drawing.Color.WhiteSmoke;
+            this.iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton2.IconSize = 30;
+            this.iconButton2.Location = new System.Drawing.Point(29, 205);
+            this.iconButton2.Name = "iconButton2";
+            this.iconButton2.Size = new System.Drawing.Size(32, 35);
+            this.iconButton2.TabIndex = 13;
+            this.iconButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Khmer OS Siemreap", 12F);
+            this.textBox2.Location = new System.Drawing.Point(70, 206);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(339, 30);
+            this.textBox2.TabIndex = 11;
+            // 
+            // DGV_DATA
+            // 
+            this.DGV_DATA.AllowUserToAddRows = false;
+            this.DGV_DATA.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DGV_DATA.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.DGV_DATA.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.DGV_DATA.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.DGV_DATA.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_DATA.Location = new System.Drawing.Point(3, 3);
+            this.DGV_DATA.Name = "DGV_DATA";
+            this.DGV_DATA.RowHeadersVisible = false;
+            this.DGV_DATA.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DGV_DATA.Size = new System.Drawing.Size(594, 294);
+            this.DGV_DATA.TabIndex = 0;
+            // 
+            // LOGIN
+            // 
+            this.LOGIN.FlatAppearance.BorderSize = 0;
+            this.LOGIN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LOGIN.Font = new System.Drawing.Font("Khmer OS Siemreap", 10F);
+            this.LOGIN.ForeColor = System.Drawing.Color.LightCoral;
+            this.LOGIN.IconChar = FontAwesome.Sharp.IconChar.Save;
+            this.LOGIN.IconColor = System.Drawing.Color.DarkSalmon;
+            this.LOGIN.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.LOGIN.IconSize = 30;
+            this.LOGIN.Location = new System.Drawing.Point(429, 89);
+            this.LOGIN.Name = "LOGIN";
+            this.LOGIN.Size = new System.Drawing.Size(159, 35);
+            this.LOGIN.TabIndex = 14;
+            this.LOGIN.Text = "រក្សាទុក | SAVE";
+            this.LOGIN.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.LOGIN.UseVisualStyleBackColor = true;
+            // 
+            // iconButton3
+            // 
+            this.iconButton3.FlatAppearance.BorderSize = 0;
+            this.iconButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton3.Font = new System.Drawing.Font("Khmer OS Siemreap", 10F);
+            this.iconButton3.ForeColor = System.Drawing.Color.LightCoral;
+            this.iconButton3.IconChar = FontAwesome.Sharp.IconChar.Edit;
+            this.iconButton3.IconColor = System.Drawing.Color.DarkSalmon;
+            this.iconButton3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton3.IconSize = 30;
+            this.iconButton3.Location = new System.Drawing.Point(429, 166);
+            this.iconButton3.Name = "iconButton3";
+            this.iconButton3.Size = new System.Drawing.Size(159, 35);
+            this.iconButton3.TabIndex = 15;
+            this.iconButton3.Text = "កែប្រែ | EDIT";
+            this.iconButton3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.iconButton3.UseVisualStyleBackColor = true;
+            // 
+            // iconButton5
+            // 
+            this.iconButton5.FlatAppearance.BorderSize = 0;
+            this.iconButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton5.Font = new System.Drawing.Font("Khmer OS Siemreap", 10F);
+            this.iconButton5.ForeColor = System.Drawing.Color.LightCoral;
+            this.iconButton5.IconChar = FontAwesome.Sharp.IconChar.TrashAlt;
+            this.iconButton5.IconColor = System.Drawing.Color.DarkSalmon;
+            this.iconButton5.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton5.IconSize = 30;
+            this.iconButton5.Location = new System.Drawing.Point(429, 229);
+            this.iconButton5.Name = "iconButton5";
+            this.iconButton5.Size = new System.Drawing.Size(159, 35);
+            this.iconButton5.TabIndex = 16;
+            this.iconButton5.Text = "លុប | DELETE";
+            this.iconButton5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.iconButton5.UseVisualStyleBackColor = true;
+            // 
+            // Frm_CreditType
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 600);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Khmer OS Siemreap", 11F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.Name = "Frm_CreditType";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Frm_CreditType";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_DATA)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private FontAwesome.Sharp.IconButton Btn_Exit;
+        private System.Windows.Forms.Panel panel5;
+        private FontAwesome.Sharp.IconButton iconButton2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel4;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel3;
+        private FontAwesome.Sharp.IconButton iconButton4;
+        private System.Windows.Forms.TextBox TXT_USERNAME;
+        private System.Windows.Forms.DataGridView DGV_DATA;
+        private FontAwesome.Sharp.IconButton iconButton5;
+        private FontAwesome.Sharp.IconButton iconButton3;
+        private FontAwesome.Sharp.IconButton LOGIN;
+    }
+}
---
+++ Loan/FORMS/Frm_CreditType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loan.FORMS
+{
+    public partial class Frm_CreditType : Form
+    {
+        public Frm_CreditType()
+        {
+            InitializeComponent();
+        }
+    }
+}
